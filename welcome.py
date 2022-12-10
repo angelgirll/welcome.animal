a4375bd51333a26026c9c18a9daf09c68c48e72e
angelgirll <119132116+angelgirll@users.noreply.github.com>
2022-12-10 22:56:17 +0330

    Add files via upload

---
+++ welcome.py
@@ -0,0 +1,5 @@
+print("welcome to my programe!!")
+your_city = input("Which city do you live in?")
+your_favarite_animal_name= input("What is your favorite animal?")
+my_group= your_city + your_favarite_animal_name
+print(my_group)
